2f217147b0ff31aca309a46832beb72aa07c13ec
etonstro <tonstrom.egert@gmail.com>
2026-03-04 14:43:45 +0200

    add: pagination

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,21 +1,45 @@
-import { Text, Anchor, Center, Loader } from "@mantine/core";
+import {
+  Text,
+  Anchor,
+  Center,
+  Loader,
+  Pagination,
+  Group,
+  Stack,
+} from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router-dom";
 import type { Ad } from "../context/AdsContext";
 import AdCard from "../components/AdCard";
 
 export default function HomePage() {
-  const {
-    data: ads,
-    isLoading,
-    isError,
-  } = useQuery<Ad[], Error>({
-    queryKey: ["ads"],
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const page = Number(searchParams.get("page")) || 1;
+  const limit = 12;
+
+  const { data, isLoading, isError } = useQuery<
+    {
+      data: Ad[];
+      totalPages: number;
+    },
+    Error
+  >({
+    queryKey: ["ads", page],
     queryFn: async () => {
-      const res = await fetch("https://surmakuulutus-back.onrender.com/ads");
+      const res = await fetch(
+        `https://surmakuulutus-back.onrender.com/ads?page=${page}&limit=${limit}`,
+      );
       if (!res.ok) throw new Error("Andmete laadimine ebaõnnestus");
       return res.json();
     },
+    placeholderData: (previousData) => previousData,
   });
+
+  const handlePageChange = (value: number) => {
+    setSearchParams({ page: value.toString() });
+  };
+
   if (isLoading) {
     return (
       <Center style={{ height: "50vh" }}>
@@ -30,13 +54,27 @@ export default function HomePage() {
 
   return (
     <>
-      {ads && ads.length === 0 && <Text>Kuulutusi pole veel lisatud.</Text>}
+      <Stack>
+        <Group>
+          {data?.data.length === 0 && (
+            <Text ta="center">Kuulutusi pole veel lisatud.</Text>
+          )}
 
-      {ads?.map((ad) => (
-        <Anchor href={`/ads/${ad._id}`} key={ad._id} underline="never">
-          <AdCard ad={ad} hoverable />
-        </Anchor>
-      ))}
+          {data?.data.map((ad) => (
+            <Anchor href={`/ads/${ad._id}`} key={ad._id} underline="never">
+              <AdCard ad={ad} hoverable />
+            </Anchor>
+          ))}
+        </Group>
+        <Group justify="center" p="lg">
+          <Pagination
+            value={page}
+            onChange={handlePageChange}
+            total={data?.totalPages || 1}
+            mt="lg"
+          />
+        </Group>
+      </Stack>
     </>
   );
 }
